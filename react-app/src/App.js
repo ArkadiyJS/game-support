08b07f3018f8e799538107f82d2b1d52117cae4b
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2024-08-28 16:31:44 +0500

    html skelet

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,8 +3,49 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      hi one
+    <div className='app'>
+
+      <div className='heroOne'>
+        <div onClick={() => console.log(`hi`)} className='setting' ></div>
+
+        <div className='ult'>первый</div>
+        <div className='bkb'>бкб</div>
+
+        <div className='refresher'>реф</div>
+      </div>
+      <div className='heroOne'>
+        <div className='setting'></div>
+        <div className='ult'>первый</div>
+        <div className='bkb'>бкб</div>
+
+        <div className='refresher'>реф</div>
+      </div>
+      <div className='heroOne'>
+        <div className='setting'></div>
+        <div className='ult'>первый</div>
+        <div className='bkb'>бкб</div>
+
+        <div className='refresher'>реф</div>
+      </div>
+      <div className='heroOne'>
+        <div className='setting'></div>
+        <div className='ult'>первый</div>
+        <div className='bkb'>бкб</div>
+
+        <div className='refresher'>реф</div>
+      </div>
+      <div className='heroOne'>
+        <div className='setting' ></div>
+        <div className='ult'>первый</div>
+        <div className='bkb'>бкб</div>
+
+        <div className='refresher'>реф</div>
+      </div>
+
+      <div className='roshan'>
+        roshan
+      </div>
+
     </div>
   );
 }
